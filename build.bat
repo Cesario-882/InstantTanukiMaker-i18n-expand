f7a6c2ec9cfd92235b1339ddd532c01f481b4586
Cesario-882 <kawakaze1@qq.com>
2026-08-30 20:08:06 +0800

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Installing dependencies...
-pip install -r requirements.txt
-echo Building executable... note: you must have the Data/ folder present.
-pyinstaller tanukora.spec
-echo Done.
-pause
